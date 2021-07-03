6112869bb189f824b5848841e8ae2ccc12f538aa
Oleg <olegjan9@rambler.ru>
2021-07-03 11:32:53 +0300

    02.07.2021

--- src/js/script.js
+++ src/js/script.js
@@ -495,5 +495,52 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     });
 
+    // Scroll
+
+    // Скрытие иконки прокрутки
+    const sroll = document.querySelector(".scroll");
+
+    window.addEventListener('scroll', () => {
+        if (document.documentElement.scrollTop < 900) {
+            sroll.classList.add("hide");
+            sroll.classList.remove("show");
+        } else {
+            sroll.classList.add("show");
+            sroll.classList.remove("hide");
+        }
+    });
+
+    // Плавная прокрутка
+
+    const up = document.querySelector('.scroll__img');
+    // 1.	Получаем переменную с ссылкой м заданный атрибутом с заглушкой #
+
+    up.addEventListener('click', (e) => {
+        // 2.	Вешаем обработчик события на эту переменную (также можно перебрать все ссылки
+        e.preventDefault();
+        const upAttribut = e.target.parentNode.getAttribute("href").substr(1);
+        // Получаем атрибут href нажимаемой ссылки. Обращаюсь через parentNode, поскольку
+        // нажимаю не на ссылку, на изображение в ней.
+        // - В обработчике событий получаем атрибут нажимаемой ссылки без значка #, 
+        // чтобы можно было получить только слово, которое присвоено к id, куда нужно переместиться.  
+        // Этот атрибут присваиваем к переменной.
+        document.getElementById(upAttribut).scrollIntoView({
+            // Полученный атрибут  с обрезанным substr(1) первым символом. помеащем в поиск по id. 
+            // Т.е. будем искать id с этим же атрибутом, но без #
+            // Создаем перемещение к этому атрибуту (месту в док. с этим же атрибутом слово)
+            // Получаем элемент, к которому мы ранее присвоили только атрибут слово без значка #, и присваиваем 
+            // к нему метод скролла c двумя параметрами .scrollIntoView({
+            // })
+            behavior: "smooth",
+            block: 'start' /* к какому месту блока нужно переместиться */
+        });
+        //     3.	Метод .scrollIntoView работает не во всех браузерах. Чтобы работал во всех на сайте 
+        // npm скачиваем файл js из репозитория smoothscroll-polyfill.
+        // npm I smoothscroll-polyfill –save но с файлом надежнее
+        // Можно этот файл (обычный файл js) напрямую скачать из репозитория и подключить его 
+        // как обычный js перед основным js
+    });
+
+
 
 });
