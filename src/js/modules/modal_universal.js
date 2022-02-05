ca9cdbf1f3960fa12703cdbf22ee1f88da5fff67
Oleg <olegjan9@rambler.ru>
2022-02-05 13:15:17 +0300

    05/02/2022

--- src/js/modules/modal_universal.js
+++ src/js/modules/modal_universal.js
@@ -1,19 +1,34 @@
 const modal_universal = () => {
+
+    // Верстка такова, что скрываем и показываем
+    // фон модального окна (его подложку)    
     //-------------------1. Функции---------------------------------------//
     // 1.1. Скрытие, показ модального окна 
     function actionModal({
         /* кнопка, открываюая мод. окно */
         selectorButton,
-        /* само модальное окно */
+        /* Подложка (фон) конкретного модального окна */
         selectorModal,
-        /* кнопка, закрывающая модальное окно */
+        /* Подложки (фон) всех модальных окон */
+        // Чтобы закрывать все окна на случай вызова модального
+        // окна через другое модальное окно, а не через верстку.
+        // В обратном случае два открытых окна будут мешаться
+        // друг другу.
+        selectorModals,
+        /* кнопка, скрывающая фон модального окна
+        вместе с модальным окном */
         selectorClose,
         /* класс, присваивающий display: block; */
         selectorShow,
+        dataModals = true,
+        dataValidation = false,
     }) {
+
         const button = document.querySelectorAll(selectorButton),
             modal = document.querySelector(selectorModal),
-        close = document.querySelectorAll(selectorClose);
+            modals = document.querySelectorAll(selectorModals),
+            close = document.querySelectorAll(selectorClose);
+
 
         // Прописать класс показывающий и скрывающий модальное окно
         function closeModal() {
@@ -23,16 +38,52 @@ const modal_universal = () => {
         }
 
         function showModal() {
+            // Все окна закрываются
+            modals.forEach(item => {
+                item.classList.remove(selectorShow);
+            });
+            // Открывается только заданное модальное окно
             modal.classList.add(selectorShow);
             // Окно прокручивается
-            document.body.style.overflow = "";
             document.body.style.overflow = "hidden";
         }
-        // Событие на несколько кнопок
+
+        
+
+
+        // Событие все кнопоки
         button.forEach(button => {
             button.addEventListener("click", (e) => {
-                e.preventDefault();
-                if (e.target) {
+                e.preventDefault();               
+                if (e.target && dataValidation) {
+                    if (e.target) {
+                        modals.forEach(item => {
+                            item.classList.remove(selectorShow);
+                        });
+                        showModal();
+                    } else {
+                        let statusMessage = document.createElement('div');
+                        statusMessage.classList.add('status');
+                        statusMessage.textContent = "Не все выбрано";
+                        document.querySelector('.popup_calc_content').appendChild(statusMessage);
+                        setTimeout(function () {
+                            statusMessage.remove();
+                        }, 2000);
+                    }
+                    if (e.target) {
+                        modals.forEach(item => {
+                            item.classList.remove(selectorShow);
+                        });
+                        showModal();
+                    } else {
+                        console.log('Нет');
+                    }
+
+                } else if (e.target && !dataValidation) {
+                    // Все окна закрываются
+                    modals.forEach(item => {
+                        item.classList.remove(selectorShow);
+                    });
                     showModal();
                 }
             });
@@ -41,7 +92,12 @@ const modal_universal = () => {
         // Клик на крестики - окно исчезает
         close.forEach(close => {
             close.addEventListener("click", (e) => {
+                // Все окна закрываются
+                modals.forEach(item => {
+                    item.classList.remove(selectorShow);
+                });
                 closeModal();
+                document.body.style.overflow = "";
             });
         });
 
@@ -49,8 +105,13 @@ const modal_universal = () => {
         modal.addEventListener("click", (e) => {
             // Если кликаем только на подложку,
             // а не на само модальное окно
-            if (e.target == modal) {
+            if (e.target === modal && dataModals) {
+                // Все окна закрываются
+                modals.forEach(item => {
+                    item.classList.remove(selectorShow);
+                });
                 closeModal();
+                document.body.style.overflow = "";
             }
         });
 
@@ -58,21 +119,26 @@ const modal_universal = () => {
         document.addEventListener('keydown', (e) => {
             if (e.code === "Escape" && modal.classList.contains(selectorShow)) {
                 closeModal();
+                document.body.style.overflow = "";
             }
         });
     }
 
     //1.2.Через время открываются не все, а конкретное окно
-    // function timerShowModal({
-    //     selectorModal,
-    //     time,
-    //     selectorShow
-    // }) {
-    //     setTimeout(function () {
-    //         document.querySelector(selectorModal).classList.add(selectorShow);
-    //     }, time);
-    // }
-    // //-------------------2. Вызовы функций---------------------------------------//
+    function timerShowModal({
+        selectorModal,
+        time,
+        selectorShow
+    }) {
+        setTimeout(function () {
+            document.querySelector(selectorModal).classList.add(selectorShow);
+        }, time);
+    }
+
+    
+
+
+    //-------------------2. Вызовы функций---------------------------------------//
     // 
     // 2.1. Вызов открытия конкретного окна через время
     // timerShowModal({
@@ -81,20 +147,57 @@ const modal_universal = () => {
     //     selectorShow: 'show'
     // });
 
-    // 2.2. Вызов одного модального окна при нажатии на одну
-    // из кнопок 
+    // 2.2. Выбрать замерщика
     actionModal({
-        selectorButton: ".popup_engineer_btn",
         /* кнопка, открываюая мод. окно */
+        selectorButton: ".popup_engineer_btn",
+        /* Подложка (фон) модального окна */
         selectorModal: '.popup_engineer',
-        /* само модальное окно */
-        selectorClose: '.popup_close',
         /* кнопка, закрывающая модальное окно */
-        selectorShow: 'show' /* класс (без точки), присваивающий display: block; */
+        selectorClose: '.popup_close',
+        /* класс (без точки), присваивающий display: block; */
+        selectorShow: 'show'
     });
 
-  
+    // 2.3. Обратный звонок
+    actionModal({
+        selectorButton: ".phone_link",
+        selectorModal: '.popup',
+        selectorClose: '.popup_close',
+        selectorShow: 'show'
+    });
 
+    // 2.4. Рассчитать стоимость
+    actionModal({
+        selectorButton: ".glazing_price_btn",
+        selectorModal: '.popup_calc',
+        /* Подложки (фон) всех модальных окон */
+        selectorModals: '[data-modals]',
+        selectorClose: '.popup_calc_close',
+        selectorShow: 'show',
+        dataModals: false
+
+    });
+
+    // 2.5. холодное или теплое остекление]
+    actionModal({
+        selectorButton: ".popup_calc_button",
+        selectorModal: '.popup_calc_profile ',
+        selectorModals: '[data-modals]',
+        selectorClose: '.popup_calc_profile_close',
+        selectorShow: 'show',
+        dataModals: false,
+        dataValidation: true,
+    });
+
+    actionModal({
+        selectorButton: ".popup_calc_profile_button",
+        selectorModal: '.popup_calc_end',
+        selectorModals: '[data-modals]',
+        selectorClose: '.popup_calc_end_close',
+        selectorShow: 'show',
+        dataValidation: true
+    });
 };
 
 export default modal_universal;
